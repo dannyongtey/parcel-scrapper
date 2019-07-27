4ec2996b5845a2536f13ab19068fce8639eabe8c
Danny Tey <dannyongtey@gmail.com>
2019-07-27 16:50:19 +0800

    feat: Add auth for google student auth

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 const api = require('./src/api');
 
 const {runSchedule} = require('./src/scheduler')
+const {authMiddleware} = require('./src/middleware/auth')
 
 app.get('/', (req, res) => res.sendStatus(200));
 app.get('/health', (req, res) => res.sendStatus(200));
@@ -26,6 +27,14 @@ app.use(
 app.use(helmet());
 
 app.use(api);
+app.use(authMiddleware)
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Credentials, Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
 
 // Run scheduler
 runSchedule()
---
+++ src/middleware/auth.js
@@ -0,0 +1,31 @@
+const { OAuth2Client } = require('google-auth-library')
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+const client = new OAuth2Client(GOOGLE_CLIENT_ID);
+
+module.exports = {
+  async authMiddleware(req, res, next) {
+    if (req.method !== 'OPTIONS') {
+      const authHeader = JSON.parse(req.header('authorization').split('Bearer ')[1])
+      const { tokenId } = authHeader
+      try {
+        const ticket = await client.verifyIdToken({
+          idToken: tokenId,
+          audience: GOOGLE_CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        if (payload['hd'] !== 'student.mmu.edu.my') {
+          res.status(401).json({ error: 'Unauthorized. Please use MMU student email to login.' })
+          return
+        } else {
+          next()
+        }
+      } catch {
+        res.status(401).json({ error: 'Unauthorized. Please ensure you are logged in using official MMU Account.' })
+        return
+      }
+    } else {
+      next()
+    }
+  }
+}
