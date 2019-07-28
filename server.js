b15ae52b73cc151884521641bf248465c1f8de85
Danny Tey <dannyongtey@gmail.com>
2019-07-28 10:04:46 +0800

    improvements: Development don't send frequent requests

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(helmet());
 app.use(api);
 app.use(authMiddleware)
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:9000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Credentials, Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
